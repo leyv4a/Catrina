a696917507c1cb58d85bf1f32d05ee683298da0f
gabri <gabri@leyv4a>
2022-10-31 21:24:28 -0700

    Primera version

--- src/mx/itson/Catrina/enumerador/Mes.java
+++ src/mx/itson/Catrina/enumerador/Mes.java
@@ -4,10 +4,38 @@
  */
 package mx.itson.Catrina.enumerador;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  *
  * @author gabri
  */
 public enum Mes {
     
+
+@SerializedName("1")
+Enero,
+@SerializedName("2")
+Febrero,
+@SerializedName("3")
+Marzo,
+@SerializedName("4")
+Abril,
+@SerializedName("5")
+Mayo,
+@SerializedName("6")
+Junio,
+@SerializedName("7")
+Julio,
+@SerializedName("8")
+Agosto,
+@SerializedName("9")
+Septiembre,
+@SerializedName("10")
+Octubre,
+@SerializedName("11")
+Noviembre,
+@SerializedName("12")
+Diciembre
+
 }
